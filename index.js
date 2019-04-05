48a8cac822c84f22a31ac28da79b8f967f909a44
MichalAW <michal.wanta@gmail.com>
2019-04-05 21:24:13 +0200

    node.js missing script problem

--- index.js
+++ index.js
@@ -1,52 +1,15 @@
-var os = require('os');
+var fs = require('fs');
 var colors = require('colors');
-var OSinfo = require('../modules/OSinfo');
- 
-console.log('hello'.green); // outputs green text
-console.log('i like cake and pies'.underline.red) // outputs red underlined text
-console.log('inverse the color'.inverse); // inverses the color
-console.log('OMG Rainbows!'.rainbow); // rainbow
-console.log('Run the trap'.trap); // Drops the bass
 
-process.stdin.setEncoding('utf-8');
-process.stdin.on('readable', function() {
-    var input = process.stdin.read();
-    if(input !== null) {
-        var instruction = input.trim();
-        switch(instruction) {
-            case '/exit':
-                process.stdout.write('Quitting app!\n');
-                process.exit();
-                break;
-            case '/sayhello':
-                process.stdout.write('hello!\n');
-                break;
-            default:
-                process.stderr.write('Wrong instruction!\n');
-            case '/sayhello':
-                process.stdout.write('hello!\n');
-            break;
-            var os = require('os');
-
-            function getOSinfo() {
-                var type = os.type();
-                if(type === 'Darwin') {
-                    type = 'OSX';
-                } else if(type === 'Windows_NT') {
-                    type = 'Windows';
-                }
-                var release = os.release();
-                var cpu = os.cpus()[0].model;
-                var uptime = os.uptime();
-                var userInfo = os.userInfo();
-                console.log('System:'.grey, type);
-                console.log('Release:'.red, release);
-                console.log('CPU model:'.blue, cpu);
-                console.log('Uptime: ~'.green, (uptime / 60).toFixed(0), 'min');
-                console.log('User name:'.yellow, userInfo.username);
-                console.log('Home dir:'.cyan, userInfo.homedir);
-            }
-            exports.print = getOSinfo;
-        };
-    }
+fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
+    console.log('Dane przed zapisem!'.blue);
+    console.log(data);
+    fs.appendFile('./tekst.txt', '\nA tak wyglądają po zapisie!', function(err) {
+        if (err) throw err;
+        console.log('Zapisano!'.blue);
+        fs.readFile('./tekst.txt', 'utf-8', function(err, data) {
+            console.log('Dane po zapisie'.blue)
+            console.log(data);
+        });
+    });
 });
