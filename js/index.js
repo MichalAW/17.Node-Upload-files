2b6d1b54dd0c9b15ec5f0f7bb95b37c0a06f9d0b
MichalAW <michal.wanta@gmail.com>
2019-04-07 12:44:28 +0200

    fs.readFile

--- js/index.js
+++ js/index.js
@@ -1,16 +1,25 @@
 var http = require('http');
+var fs = require('fs');
 var server = http.createServer();
 
 server.on('request', function (request, response) {
-    response.setHeader("Content-Type", "text/html; charset=utf-8");
-    if (request.method === 'GET' && request.url === '/hello') {
-        response.write('<h1>Hello World!</h1>');
-        response.end();
-    } else {
-        response.statusCode = 404;
-        response.write('<h1>404: Zła ścieżka!</h1>');
-        response.end();
-    }
+	response.setHeader('Content-Type', 'text/html; charset=utf-8');
+	if (request.method === 'GET' && request.url === '/hello') {
+		// First I want to read the file
+		fs.readFile('../index.html', function (error, data) {
+		    if (error) throw error;
+		    response.write(data);
+			response.end();
+		});
+	} else {
+		fs.readFile('../images/cat.jpeg', function (error, data) {
+		  	if (error) throw error;
+		    response.statusCode = 404;
+		  	response.write('<html><body><img src="data:image/jpeg;base64,')
+		  	response.write(Buffer.from(data).toString('base64'));
+		  	response.end('"/></body></html>');
+		});
+	}
 });
 
 server.listen(8080);
