896d13120d877dec5d25813493d28934822d93e3
MichalAW <michal.wanta@gmail.com>
2019-04-07 11:12:20 +0200

    fix colors-task finished

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -13,12 +13,12 @@ function getOSinfo() {
 		type = 'Windows';
 	}
 
-	console.log('System: ' , type);
-	console.log('Release: ', release);
-	console.log('CPU model: ', cpu);
-	console.log('Uptime: ', (uptime / 60).toFixed(0), 'min');
-	console.log('User name: ', userInfo.username);
-	console.log('Home dir: ', userInfo.homedir);
+    console.log('System:'.grey, type);
+    console.log('Release:'.red, release);
+    console.log('CPU model:'.blue, cpu);
+    console.log('Uptime: ~'.green, (uptime / 60).toFixed(0), 'min');
+    console.log('User name:'.yellow, userInfo.username);
+    console.log('Home dir:'.cyan, userInfo.homedir);
 }
 
 exports.print = getOSinfo;
