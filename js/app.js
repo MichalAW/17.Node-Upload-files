3435eb035ddbe39b8f408d67d2092d4596bfc48f
MichalAW <michal.wanta@gmail.com>
2019-04-07 11:25:27 +0200

    fix js and app

--- js/app.js
+++ js/app.js
@@ -1,24 +1,23 @@
-var os = require('os');
-var colors = require('colors');
-function getOSinfo() {
-	var type = os.type();
-	var release = os.release();
-	var cpu = os.cpus()[0].model;
-	var uptime = os.uptime();
-	var userInfo = os.userInfo();
+var OSinfo = require('../modules/OSinfo');
 
-	if(type === 'Darwin') {
-		type = 'OSX';
-	} else if(type === 'Windows_NT') {
-		type = 'Windows';
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function() {
+	var input = process.stdin.read();
+	if (input !== null) {
+		var instruction = input.trim();
+		switch(instruction) {
+			case '/exit':
+				process.stdout.write('Quitting app!\n');
+				process.exit()
+				break;
+			case '/sayhello':
+				process.stdout.write('hello!\n');
+				break;
+			case '/getOSinfo':
+				// informacje o systemie
+				OSinfo.print();
+			default:
+				process.stderr.write('Wrong instruction!\n');
+		};
 	}
-
-    console.log('System:'.grey, type);
-    console.log('Release:'.red, release);
-    console.log('CPU model:'.blue, cpu);
-    console.log('Uptime: ~'.green, (uptime / 60).toFixed(0), 'min');
-    console.log('User name:'.yellow, userInfo.username);
-    console.log('Home dir:'.cyan, userInfo.homedir);
-}
-
-exports.print = getOSinfo;
+});
