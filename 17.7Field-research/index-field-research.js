ca228d52c33101dd646749871bb8cc74d2af2b33
MichalAW <michal.wanta@gmail.com>
2019-04-12 00:54:38 +0200

    delete files

--- 17.7Field-research/index-field-research.js
+++ 17.7Field-research/index-field-research.js
@@ -2,18 +2,9 @@ var fs = require('fs');
 var StatMode = require('stat-mode');
 var colors = require('colors');
 
-// fs.stat('./img/cat.jpeg', function(error, stats) {
-//  var statMode = new StatMode(stats);
-//  console.log(statMode.toString());
-// });
-
-// fs.readFile('./tekst.txt', function(error, data) {
-//  console.log(data);
-// });
-
 fs.writeFile('./tekst.txt', 'Tekst, który zapiszemy w pliku tekst.txt',
 function(error) {
-    if (error) throw error; // jeśli pojawi się błąd, wyrzuć wyjątek
+    if (error) throw error; // if an error occurs, throw an exception
     console.log('Zapisano!');
 });
 
