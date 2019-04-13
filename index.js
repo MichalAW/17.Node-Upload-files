958c73a9d34c0fd5954828e87b705ea1c5597bde
MichalAW <michal.wanta@gmail.com>
2019-04-13 03:17:01 +0200

    clean

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ server.on('request', function (request, response) {
 	response.setHeader('Content-Type', 'text/html; charset=utf-8');
 	if (request.method === 'GET' && request.url === '/hello') {
 		// First I want to read the file
-		fs.readFile('../index.html', function (error, data) {
+		fs.readFile('./index.html', function (error, data) {
 		    if (error) throw error;
 		    response.write(data);
 			response.end();
 		});
 	} else {
-		fs.readFile('../images/cat.jpg', function (error, data) {
+		fs.readFile('./cat.jpg', function (error, data) {
 		  	if (error) throw error;
 		    response.statusCode = 404;
 		  	response.write('<html><body><img src="data:image/jpg;base64,')
