a2421b7d039e2c1a4237093267427e2156ad707c
MichalAW <michal.wanta@gmail.com>
2019-04-23 18:58:20 +0200

    fix

--- app/program.js
+++ app/program.js
@@ -3,23 +3,24 @@ var OSinfo = require('../modules/OSinfo');
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
     var input = process.stdin.read();
-    switch (input !== null) {
-        var instruction = input.trim();
-        case :
-            switch (instruction) {
-                case '/exit':
-                    process.stdout.write('Quitting app!\n');
-                    process.exit();
-                break;
-                case '/sayhello':
-                    process.stdout.write('hello!\n');
-                break;
-                case '/getOSinfo':
-                // system information
-                    OSinfo.print();
-                default:
-                    process.stderr.write('Wrong instruction!\n');
-            };
-        break;
+    var inputforSwitch = (input !== null);
+    var instruction = input.trim();
+    
+    switch (iinputforSwitch) {
+        switch (instruction) {
+            case '/exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+            break;
+            case '/sayhello':
+                process.stdout.write('hello!\n');
+            break;
+            case '/getOSinfo':
+            // system information
+                OSinfo.print();
+            default:
+                process.stderr.write('Wrong instruction!\n');
+        };
+    break;
     }
 });
