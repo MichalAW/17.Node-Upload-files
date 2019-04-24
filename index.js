4fb9fa797822050b688ff8c6128a6ff427bbc129
MichalAW <michal.wanta@gmail.com>
2019-04-24 22:47:32 +0200

    add new line handlers.js

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 var server = require('./modules/server');
-var express = require('express');
-var app = express();
-
-app.use(express.static('css'));
 
 server.start();
--- modules/handlers.js
+++ modules/handlers.js
@@ -5,7 +5,7 @@ exports.upload = function(request, response) {
     console.log("Rozpoczynam obsługę żądania upload.");
     var form = new formidable.IncomingForm();
     form.parse(request, function(error, fields, files) {
-        fs.renameSync(files.upload.path, "test.png");
+        fs.renameSync(files.upload.path, "test.jpg");
         response.writeHead(200, {"Content-Type": "text/html"});
         response.write("received image:<br/>");
         response.write("<img src='/show' />");
@@ -20,11 +20,16 @@ exports.welcome = function(request, response) {
         response.write(html);
         response.end();
     });
+    fs.readFile('css/style.css', function(err, css) {
+        response.writeHead(200, {"Content-Type": "text/css; charset=utf-8"});
+        response.write(css);
+        response.end();
+    });
 }
 
 exports.show = function(request, response) {
     fs.readFile("test.png", "binary", function(error, file) {
-        response.writeHead(200, {"Content-Type": "image/png"});
+        response.writeHead(200, {"Content-Type": "image/jpg"});
         response.write(file, "binary");
         response.end();
     });
