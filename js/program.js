60ffdab16c055b02044abebc1cc81996a5d32786
MichalAW <michal.wanta@gmail.com>
2019-04-27 07:41:30 +0200

    fix

--- js/program.js
+++ js/program.js
@@ -2,9 +2,12 @@ process.stdin.on('readable', function() {
 // the .read () method is to read what the user has given at the input
     var input = process.stdin.read();
     var instruction = input.toString().trim();
-    var inputForSwitch = input !== null;
-    switch(inputForSwitch) {
-        case instruction === '/exit':
+    // input must be 'true'
+    if (!input) {
+        return;
+    }
+    switch(instruction) {
+        case '/exit':
             process.stdout.write('Quitting app!\n');
             process.exit();
             break;
