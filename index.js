614c0fc5a35087e77905193aa05655898e46c154
MichalAW <michal.wanta@gmail.com>
2019-04-27 08:20:16 +0200

    fix

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var fs = require('fs');
 var server = http.createServer();
 
 server.on('request', function (request, response) {
-	response.setHeader('Content-Type', 'text/html; charset=utf-8');
 	if (request.method === 'GET' && request.url === '/hello') {
+        response.setHeader('Content-Type', 'text/html; charset=utf-8');
 		// First I want to read the file
 		fs.readFile('./index.html', function (error, data) {
 		    if (error) throw error;
@@ -12,12 +12,12 @@ server.on('request', function (request, response) {
 			response.end();
 		});
 	} else {
-		fs.readFile('./cat.jpg', function (error, data) {
+        response.setHeader('Content-Type', 'image/png; charset=utf-8');
+		fs.readFile('./cat.png', function (error, data) {
 		  	if (error) throw error;
 		    response.statusCode = 404;
-		  	response.write('<html><body><img src="data:image/jpg;base64,')
-		  	response.write(Buffer.from(data).toString('base64'));
-		  	response.end('"/></body></html>');
+		  	response.write(data);
+		  	response.end();
 		});
 	}
 });
