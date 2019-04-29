f3d3c97a88bd41774814d1c2c505751c9267d670
MichalAW <michal.wanta@gmail.com>
2019-04-29 23:50:05 +0200

    again gix

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ server.on('request', function (request, response) {
     if (request.method === 'GET' && request.url === '/hello') {
         response.setHeader('Content-Type', 'text/html; charset=utf-8');
         // First I want to read the file
-		fs.readFile('./index.html', function (error, data) {
+        fs.readFile('./index.html', function (error, data) {
             if (error) throw error;
             response.write(data);
             response.end();
-		});
+        });
 	} else {
         response.setHeader('Content-Type', 'image/png; charset=utf-8');
         fs.readFile('./cat.png', function (error, data) {
@@ -18,8 +18,8 @@ server.on('request', function (request, response) {
             response.statusCode = 404;
             response.write(data);
             response.end();
-		});
-	}
+        });
+    }
 });
 
 server.listen(8080);
