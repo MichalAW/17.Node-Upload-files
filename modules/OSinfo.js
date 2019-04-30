1ba3ba5eec32b922e11d1a70af4849bba412bee4
MichalAW <michal.wanta@gmail.com>
2019-04-30 16:46:58 +0200

    fix

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -5,6 +5,8 @@ function getOSinfo() {
 	var release = os.release();
 	var cpu = os.cpus()[0].model;
 	var uptime = os.uptime();
+	var uptimeSeconds = os.uptimeSeconds();
+	var uptimeHours = os.uptimeHours(); 
 	var userInfo = os.userInfo();
 
 	if (type === 'Darwin') {
@@ -17,6 +19,8 @@ function getOSinfo() {
 	console.log('Release: ', release);
 	console.log('CPU model: ', cpu);
 	console.log('Uptime: ', (uptime / 60).toFixed(0), 'min');
+	console.log('UptimeSeconds: ', (uptimeSeconds / 125).toFixed(2 min 5 sec), 'min');
+	console.log('UptimeHours : ', (uptimeHours / 3700).toFixed(1 h 40 sec), 'h');
 	console.log('User name: ', userInfo.username);
 	console.log('Home dir: ', userInfo.homedir);
 };
