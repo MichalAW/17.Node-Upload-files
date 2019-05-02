3c77488ec6b273860ee17f1c8017c6b2cf238b8e
MichalAW <michal.wanta@gmail.com>
2019-05-02 21:30:49 +0200

    repeat task

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -1,28 +1,25 @@
 var os = require('os');
-var colors = require('colors');
+var time = require('../modules/time');
+
 function getOSinfo() {
-	var type = os.type();
-	var release = os.release();
-	var cpu = os.cpus()[0].model;
-	var uptime = os.uptime();
-	var uptimeSeconds = os.uptimeSeconds();
-	var uptimeHours = os.uptimeHours(); 
-	var userInfo = os.userInfo();
+    var type = os.type();
 
-	if (type === 'Darwin') {
-		type = 'OSX';
-	} else if (type === 'Windows_NT') {
-		type = 'Windows';
-	}
+    if (type === 'Darwin') {
+        type = 'OSX';
+    } else if (type === 'Windows_NT') {
+        type = 'Windows';
+    }
 
-	console.log('System: ' , type);
-	console.log('Release: ', release);
-	console.log('CPU model: ', cpu);
-	console.log('Uptime: ', (uptime / 60).toFixed(0), 'min');
-	console.log('UptimeSeconds: ', (uptimeSeconds / 125).toFixed(2 min 5 sec), 'min');
-	console.log('UptimeHours : ', (uptimeHours / 3700).toFixed(1 h 40 sec), 'h');
-	console.log('User name: ', userInfo.username);
-	console.log('Home dir: ', userInfo.homedir);
+    var release = os.release();
+    var cpu = os.cpus()[0].model;
+    var userInfo = os.userInfo();
+
+    console.log('System: ' , type);
+    console.log('Release: ', release);
+    console.log('CPU model: ', cpu);
+    console.log('User name: ', userInfo.username);
+    console.log('Home dir: ', userInfo.homedir);
+    time.newTime();
 };
 
-exports.print = getOSinfo;
+exports.print = getOSinfo;
