97022783c63844a9955a603991a091597aa641e7
MichalAW <michal.wanta@gmail.com>
2019-05-04 19:42:03 +0200

    fix

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -17,9 +17,9 @@ function getOSinfo() {
     console.log('System: ' , type);
     console.log('Release: ', release);
     console.log('CPU model: ', cpu);
+    console.log('Uptime: ' + time.timeConvertion(os.uptime()));
     console.log('User name: ', userInfo.username);
     console.log('Home dir: ', userInfo.homedir);
-    time.newTime();
 };
 
 exports.print = getOSinfo;
--- modules/time.js
+++ modules/time.js
@@ -1,27 +1,23 @@
 var os = require('os');
 
-function newTime() {
-    var uptime = os.uptime();
-
-    var h = parseInt(uptime / 3600);
-    var s = (uptime % 3600);
+function timeConvertion(numSec) {
+    // Caluculation
+    var h = parseInt(numSec / 3600);
+    var s = (numSec % 3600);
     var min = parseInt(s / 60);
-    var setMinutes = min > 0
-    var setHours = h > 0
-
-    if (setMinutes) {
-        if (setHours) {
-            {
-                console.log(`Uptime: ${h } h ${min} min ${s} s`)
-            }
-        } else {
-            {
-                console.log(`Uptime: ${min} mins ${s} s`)
-            }
-        }
-    } else {
-        console.log(`Uptime: ${s} s`)
+    var sec = s % 60;
+    
+    var result = sec + 'sec';
+    
+    if (min > 0) {
+        result = min + 'min ' + result;
     }
+
+    if (h > 0) {
+        result = h + 'h ' + result;
+    }
+    // var result = h + 'h ' + min + 'min ' + sec + 'sec'; 
+    return result; // score of calculation
 }
 
-exports.newTime = newTime;
+exports.timeConvertion = timeConvertion;
